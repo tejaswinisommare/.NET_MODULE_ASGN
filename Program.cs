613767686591e9a0e9464feea00d3daa6f45d85d
tejaswinisommare <65150881+tejaswinisommare@users.noreply.github.com>
2020-12-02 21:36:23 +0530

    ASGN 2_Inheritance&Abstract

--- Program.cs
+++ Program.cs
@@ -1,177 +1,225 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ASGN1
+namespace Assigment2
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
-
-            Employee o1 = new Employee("Teju", 123465, 10);
-            Employee o2 = new Employee("Teju", 123465);
-            Employee o3 = new Employee("Teju");
-            Employee o4 = new Employee();
 
 
-            Console.WriteLine();
-            Console.WriteLine(o1.EMPNO);
-            Console.WriteLine(o1.GetNetSalary());
-            Console.WriteLine();
-            Console.WriteLine(o2.EMPNO);
-            Console.WriteLine(o2.GetNetSalary());
-            Console.WriteLine();
-            Console.WriteLine(o3.EMPNO);
-            Console.WriteLine(o3.GetNetSalary());
-            Console.WriteLine();
-            Console.WriteLine(o4.EMPNO);
-            Console.WriteLine(o4.GetNetSalary());
-            Console.WriteLine("===================");
-
-            Console.WriteLine();
-            Console.WriteLine(o4.EMPNO);
-            Console.WriteLine(o4.GetNetSalary());
-            Console.WriteLine();
-            Console.WriteLine(o3.EMPNO);
-            Console.WriteLine(o3.GetNetSalary());
-            Console.WriteLine();
-            Console.WriteLine(o2.EMPNO);
-            Console.WriteLine(o2.GetNetSalary());
-            Console.WriteLine();
-            Console.WriteLine(o1.EMPNO);
-            Console.WriteLine(o1.GetNetSalary());
-            Console.WriteLine();
-
+            Console.WriteLine("manager");
+            Manager m1 = new Manager("Teju",1,70000, "HR");
+            Console.WriteLine("name " + m1.Name);
+            Console.WriteLine("net salary == " + m1.CalcNetSalary());
+            Console.WriteLine("===========================================");
+            Console.WriteLine("general manager");
+            GeneralManager gm1 = new GeneralManager("mahesh", 1,100000, "GM", "abccc");
+            Console.WriteLine("name " + gm1.Name);
+            Console.WriteLine("net salary == " + gm1.CalcNetSalary());
+            Console.WriteLine("===========================================");
+            Console.WriteLine("CEO");
+            CEO c1 = new CEO("yamini",4,60000);
+            Console.WriteLine("name " + c1.Name);
+            Console.WriteLine("net salary == " + c1.CalcNetSalary());
+            Console.WriteLine("===========================================");
             Console.ReadLine();
+
 
 
         }
     }
-    class Employee
+
+    public abstract class Employee
     {
 
-
-      
-        public Employee(String NAME="Noname", decimal BASIC=20000, short DEPTNO=0)
+        public Employee(String Name = "Noname", short DeptNo = 0, Decimal Basic = 20000)
         {
-            this.EmpNo = ++lastempno;
-            this.NAME = NAME;
-            this.BASIC = BASIC;
-            this.DEPTNO = DEPTNO;
+            Empno = ++empno;
+            this.Name = Name;
+            this.DeptNo = DeptNo;
+            this.Basic = Basic;
 
-           // Console.WriteLine("Employee no ="+lastempno);
-            //Console.WriteLine(NAME + "" + BASIC + "" + DEPTNO);
+
         }
 
-        #region properties
-        private string name;
+        public Employee()
+        {
+            Empno = ++empno;
+        }
 
-        public string NAME
+
+
+        private String name;
+
+        public String Name
         {
             set
             {
-                if (value != "")
+
+                if (value != null)
                 {
                     name = value;
                 }
                 else
                 {
-
-                    Console.WriteLine("wrong input!!! please enter valid input!!");
-
+                    Console.WriteLine("invalide input ...!");
                 }
-
             }
             get
             {
-
                 return name;
             }
-
         }
-        #endregion
 
-        #region properties
-        private decimal Basic;
+        private short deptno;
 
-        public decimal BASIC
-        {
-
-            set
-            {
-                if (value > 100000 && value < 999999)
-                {
-                    Basic = value;
-                }
-                else
-                {
-                    Console.WriteLine("invalide basic salary");
-                }
-            }
-            get
-            {
-
-                return Basic;
-            }
-
-        }
-        #endregion
-
-        #region properties
-        private short DeptNo;
-
-        public short DEPTNO
+        public short DeptNo
         {
             set
             {
-                if (value > 0)
+                if(value > 0)
                 {
-                    DeptNo = value;
+                    deptno = value;
                 }
                 else
                 {
-                    Console.WriteLine("invalide department No");
+                    Console.WriteLine("NO valide Deptno");
                 }
             }
             get
             {
-
-                return DeptNo;
+                return deptno;
             }
         }
-        #endregion
 
+        private decimal basic;
 
-        #region properties
-
-        private static int lastempno=0;
-        private int EmpNo;
-
-        public int EMPNO
+        public abstract decimal Basic
         {
+            get;
+            set;
+           
+        }
 
+        
+        private static int empno=0;
+
+        private int Empno;
+
+        public int EmpNo
+        {
             get
             {
-                return EmpNo;
+                return Empno;
             }
         }
-        #endregion
 
-        #region method
-        public decimal GetNetSalary()
+        public abstract decimal CalcNetSalary();
+
+    }
+
+
+    public class Manager : Employee
+    {
+
+        public Manager(string Name,short DeptNo,decimal Basic,string Designation) : base(Name,DeptNo,Basic)
         {
-            decimal basic = 100000;
+            this.Name = Name;
+            this.DeptNo = DeptNo;
+            this.Basic = Basic;
+            this.Designation = Designation;
+                
+        }
+
+           
+        public override decimal CalcNetSalary()
+        {
             decimal hra = 50000;
-            decimal da = 40000;
+            decimal da = 60000;
 
-            decimal netSalary = basic + hra + da;
+            decimal netSalary = Basic + hra + da;
 
             return netSalary;
+            
         }
-        #endregion
+
+
+        private string designation;
+
+        public string Designation
+        {
+            set
+            {
+                value = designation;
+
+            }
+            get
+            {
+                return designation;
+            }
+        }
+
+        public override decimal Basic {
+           
+            set;
+            get;
+            
+        
+        }
+    }
+
+    public class GeneralManager : Manager
+    {
+
+        public GeneralManager(String Name,short DeptNo, decimal Basic,String Designation,String Perks):base(Name,DeptNo,Basic,Designation)
+        {
+            this.Name = Name;
+            this.DeptNo = DeptNo;
+            this.Basic = Basic;
+            this.Designation = Designation;
+            this.Perks = Perks;
+
+
+        }
+        private string perks;
+
+        public string Perks
+        {
+            set;
+            get;
+        }
+    }
+
+    public class CEO : Employee
+    {
+
+        public CEO(String Name,short DeptNO, decimal Basic): base(Name,DeptNO,Basic)
+        {
+            this.Name = Name;
+            this.DeptNo = DeptNo;
+            this.Basic = Basic;
+
+        }
+        public override decimal Basic { 
+            
+            set;
+            get;
+        }
+
+        public sealed override decimal CalcNetSalary()
+        {
+            decimal hra = 50000;
+            decimal da = 60000;
+
+            decimal netSalary = Basic + hra + da;
+
+            return netSalary;
+
+        }
     }
 }
