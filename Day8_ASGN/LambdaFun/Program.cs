2ceaa05ef2c6723d93053baae4a63734505c66f3
tejaswinisommare <tejaswinisommare@gmail.com>
2020-12-08 22:07:29 +0530

    Day8 Assingment Uploaded

---
+++ Day8_ASGN/LambdaFun/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaFunction
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+
+
+            Func<decimal, decimal, decimal, decimal> SI = (P, N, R) => (P * N * R ) / 100;
+
+            Console.WriteLine(SI(20,5,60));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Func<int, int,bool> IG = (a,b) => (a > b);
+            Console.WriteLine(IG(20,10));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Predicate<int> IE = IsEven;
+            Console.WriteLine(IE(10));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Func<Employee, decimal> BS = e1 => e1.BasicSalary;
+            Console.WriteLine("Basic Salary = " + BS(e));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Predicate<Employee> BG = e1 => (e1.BasicSalary > 10000);
+            Console.WriteLine(BG(e));
+
+
+
+        }
+        static decimal SimpleInterest(decimal P, decimal N, decimal R)
+        {
+            return (P * N * R) / 100;
+        }
+
+        static bool IsGreater(int a, int b)
+        {
+            if (a > b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+
+    }
+
+
+
+    class Employee
+    {
+        public decimal BasicSalary = 20000;
+
+
+
+        static decimal GetBasic(Employee e)
+        {
+            return e.BasicSalary;
+        }
+
+
+        static bool IsGreaterThan10000(Employee e)
+        {
+            if (e.BasicSalary > 10000)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
